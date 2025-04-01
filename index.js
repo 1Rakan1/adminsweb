0617dbf7b8f1e2a01fa129d63c2525ccd267c7a0
1Rakan1 <trzmostafa11071@gmail.com>
2025-04-01 14:19:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { randomInt } from 'crypto';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 22782;
 app.use(express.json());
 
 let verificationCode = '';  // حفظ كود التحقق مؤقتًا
