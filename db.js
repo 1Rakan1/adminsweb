31ea4e04eafc223bc8beb5e9ea39e1db4c370355
1Rakan1 <trzmostafa11071@gmail.com>
2025-04-01 14:21:23 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config(); // تحميل متغيرات البيئة من ملف .env
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.log('Error connecting to MongoDB:', err);
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-export default mongoose;
+    console.log('MongoDB Connected Successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // إنهاء التطبيق في حال فشل الاتصال
+  }
+};
+
+export default connectDB;
