08ffb77350b8d53204ec8984217705c82f174c99
1Rakan1 <trzmostafa11071@gmail.com>
2025-04-01 12:28:34 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,15 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-// تحميل المتغيرات البيئية من ملف .env
 dotenv.config();
 
-// إنشاء دالة للاتصال بقاعدة البيانات
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected successfully');
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to MongoDB');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1);
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // إنهاء البرنامج في حالة فشل الاتصال
   }
 };
 
