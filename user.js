be2ad5240e4332816837e00165c95bd0a7e3931a
1Rakan1 <trzmostafa11071@gmail.com>
2025-04-01 13:26:50 +0300

    Update user.js

--- user.js
+++ user.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   discordUsername: { type: String, required: true },
-  role: { type: String, default: 'user' },  // أو admin أو superAdmin
-  createdAt: { type: Date, default: Date.now }
+  email: { type: String, required: true, unique: true },
+  role: { type: String, enum: ['user', 'admin', 'superAdmin'], default: 'user' },
+  verificationCode: { type: Number, required: true },  // كود التحقق
+  verified: { type: Boolean, default: false },  // حالة التحقق
 });
 
 const User = mongoose.model('User', userSchema);
-
 export default User;
