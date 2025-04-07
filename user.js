ea3f564c18ea7f1ba62fa69e0b088d34beb615bc
1Rakan1 <trzmostafa11071@gmail.com>
2025-04-07 21:05:22 +0300

    Create user.js

---
+++ user.js
@@ -0,0 +1,15 @@
+{
+  "management_title": "إدارة المستخدمين",
+  "username": "اسم المستخدم",
+  "current_role": "الرتبة الحالية",
+  "actions": "الإجراءات",
+  "fetch_error": "فشل في جلب بيانات المستخدمين",
+  "promote_success": "تم ترقية المستخدم بنجاح",
+  "promote_error": "فشل في ترقية المستخدم",
+  "roles": {
+    "user": "مستخدم",
+    "admin": "أدمن",
+    "assistant": "مساعد",
+    "leader": "قائد"
+  }
+}
